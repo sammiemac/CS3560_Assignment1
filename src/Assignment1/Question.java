9dab3d05bfd31d537c96369d28a5e134a006a3eb
Sam M <snv.macaraniag@gmail.com>
2023-10-04 12:14:44 -0700

    Cleaning up code

--- src/Assignment1/Question.java
+++ src/Assignment1/Question.java
@@ -3,21 +3,21 @@ package Assignment1;
 import java.util.*;
 
 public class Question{
-	LinkedHashMap<String, Boolean> question = new LinkedHashMap<>();
+	LinkedHashMap<String, Integer> question = new LinkedHashMap<>();
 	
 	// Given an integer, determine what kind of question is being created
 	public Question(int type) throws Exception {
 		// Question Type 1: Multiple choice
 		if(type == 1) {
-			question.put("A", false);
-			question.put("B", false);
-			question.put("C", false);
-			question.put("D", false);
+			question.put("A", 0);
+			question.put("B", 0);
+			question.put("C", 0);
+			question.put("D", 0);
 		
 		// Question Type 2: Yes or No
 		} else if(type == 2) {
-			question.put("Yes", false);
-			question.put("No", false);
+			question.put("Yes", 0);
+			question.put("No", 0);
 		
 		// Throws an exception if the integer given is invalid
 		} else {
@@ -25,38 +25,4 @@ public class Question{
 			
 		}
 	}
-	
-	// Change the value of a key given at the index
-	public void setAnswer(int key, boolean value) {
-		try {
-			// Sets the question hashmap to a list for indexing
-			List keys = new ArrayList(question.keySet());
-			// Given the index, find the corresponding key (converted to a string type) then change the value
-			question.put(keys.get(key).toString(), value);
-			
-		// Throws an exception if the answer couldn't be set, such as by invalid index
-		} catch (Exception e){
-			System.out.println("Error: Couldn't set answer.");
-			
-		}
-	}
-	
-	// Prints all the keys and values of the question
-	public void printAllQuestions() {
-		for (Map.Entry<String, Boolean> mapElement : question.entrySet()) {
-			String key = mapElement.getKey();
-			boolean value = mapElement.getValue();
-			System.out.println("Key: " + key + ", Value: " + value);
-			
-		}
-	}
-	
-	// Prints a single key and value pair at the given index
-	public void printQuestion(int index) {
-		List keys = new ArrayList(question.keySet());
-		System.out.println("Key and Value at index " + index + ": " + keys.get(index)
-		+ ", " + question.get(keys.get(index)));
-		
-	}
-
 }
--- src/Assignment1/SimulationDriver.java
+++ src/Assignment1/SimulationDriver.java
@@ -5,10 +5,6 @@ public class SimulationDriver {
 	public static void main(String[] args) throws Exception {
 
 		Question test = new Question(1);
-		test.printAllQuestions();
-		test.printQuestion(2);
-		test.setAnswer(2, true);
-		test.printQuestion(2);
 		
 		Student sam = new Student("013804193");
 		Student dev = new Student();
